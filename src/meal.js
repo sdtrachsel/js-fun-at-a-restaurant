ba5b4c6fa85711f14ddc78bdeb2d723bd9855cf5
Sharie Trachsel <strachsel22@turing.edu>
2023-02-02 12:55:22 -0600

    Add meal funcitons

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,47 @@
-function nameMenuItem() {
+function nameMenuItem(foodName){
+  return `Delicious ${foodName}`;
+};
 
-}
+function createMenuItem (menuItemName, price, type){
+  var newMenuItem = {
+    name: menuItemName,
+    price: price,
+    type: type, 
+  }
+  return newMenuItem;
+};
 
+function addIngredients(addFood, ingredients){
+  // if addFood is not in ingredients push else do nothing
+  if(!ingredients.includes(addFood)){
+    ingredients.push(addFood);
+  };
+};
+
+function formatPrice(initalPrice){
+  return `$${initalPrice}`;
+};
+
+function decreasePrice(initalPrice){
+  return initalPrice * .9
+};
+
+function createRecipe(title, ingredients, type){
+  var recipe = {
+    title: title, 
+    ingredients: ingredients,
+    type: type
+  }
+  return recipe;
+};
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
 
 
