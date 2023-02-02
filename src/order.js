77e8f6839b56205b2a23716600db38c0dea6e342
Sharie Trachsel <strachsel22@turing.edu>
2023-02-02 16:31:28 -0600

    Add order functions

--- src/order.js
+++ src/order.js
@@ -1,7 +1,41 @@
+function takeOrder(orderObject, ordersArray) {
+  if (ordersArray.length < 3) {
+    ordersArray.push(orderObject)
+  };
+};
+
+function refundOrder(order, ordersArray) {
+  for (i = 0; i < ordersArray.length; i++) {
+    if (order === ordersArray[i].orderNumber) {
+      var index = ordersArray.indexOf(ordersArray[i]);
+      ordersArray.splice(index, 1);
+    };
+  };
+};
+
+function listItems(ordersArray) {
+  var itemString = '';
+  for (i = 0; i < ordersArray.length; i++) {
+    if (i === (ordersArray.length - 1)) {
+      itemString += `${ordersArray[i].item}`;
+    } else {
+      itemString += `${ordersArray[i].item}, `
+    };
+  }; 
+  return itemString;
+};
+
+function searchOrder(ordersArray, orderName){
+  var itemArray = [];
+  for (i = 0; i < ordersArray.length; i++) {
+      itemArray += ordersArray[i].item;
+  }; 
+  return itemArray.includes(orderName);
+};
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
