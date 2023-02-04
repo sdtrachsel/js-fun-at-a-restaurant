2d62c05fb061faf42057dee2071d9be4dd500ce6
Sharie Trachsel <strachsel22@turing.edu>
2023-02-04 08:54:32 -0600

    refactor restaurant functions

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,71 +1,30 @@
-function createRestaurant(restuarantName) {
+function createRestaurant(restaurantName) {
   var restaurant = {
-    name: restuarantName,
+    name: restaurantName,
     menus: { breakfast: [], lunch: [], dinner: [] },
   };
   return restaurant;
 };
 
-function addMenuItem(restaurant, item) {
-  if (item.type === 'breakfast') {
-    if (!restaurant.menus.breakfast.includes(item)) {
-      restaurant.menus.breakfast.push(item);
-    };
-  } else if (item.type === 'lunch') {
-    if (!restaurant.menus.lunch.includes(item)) {
-      restaurant.menus.lunch.push(item)
-    };
-  } else {
-    if (!restaurant.menus.dinner.includes(item)) {
-      restaurant.menus.dinner.push(item)
-    };
-  };
-}
-
-
-function removeMenuItem(restaurant, item, menuType) {
-  if (menuType === 'breakfast') {
-    if (restaurant.menus.breakfast.length === 0) {
-      return `Sorry, we don't sell ${item}, try adding a new recipe!`;
-    } else {
-      for (var i = 0; i < restaurant.menus.breakfast.length; i++) {
-        if (restaurant.menus.breakfast[i].name === item) {
-          restaurant.menus.breakfast.splice(i, 1);
-        } else {
-          return `Sorry, we don't sell ${item}, try adding a new recipe!`;
-        }
-      }
-    }
-  } else if (menuType === 'lunch') {
-    if (restaurant.menus.dinner.length === 0) {
-      return `Sorry, we don't sell ${item}, try adding a new recipe!`;
-    } else {
-      for (var i = 0; i < restaurant.menus.lunch.length; i++) {
-        if (restaurant.menus.lunch[i].name === item) {
-          restaurant.menus.lunch.splice(i, 1);
-        } else {
-          return `Sorry, we don't sell ${item}, try adding a new recipe!`;
-        }
-      }
-    }
-  } else {
-    if (restaurant.menus.dinner.length === 0) {
-      return `Sorry, we don't sell ${item}, try adding a new recipe!`;
-    } else {
-      for (var i = 0; i < restaurant.menus.dinner.length; i++) {
-        if (restaurant.menus.dinner[i].name === item) {
-          restaurant.menus.dinner.splice(i, 1);
-        } else {
-          return `Sorry, we don't sell ${item}, try adding a new recipe!`;
-        }
-      }
-    }
+function addMenuItem(restaurant, menuItem) {
+  if (!restaurant.menus[menuItem.type].includes(menuItem)) {
+    restaurant.menus[menuItem.type].push(menuItem)
   }
-  return `No one is eating our ${item} - it has been removed from the ${menuType} menu!`;
 };
 
-
-
+function removeMenuItem(restaurant, menuItemName, menuItemType) {
+  var menuItems = [];
+  for (i = 0; i < restaurant.menus[menuItemType].length; i++)(
+    menuItems.push(restaurant.menus[menuItemType][i].name)
+  )
+  if (menuItems.includes(menuItemName)) {
+    var indexOfMenuItem = menuItems.indexOf(menuItemName)
+    restaurant.menus[menuItemType].splice(indexOfMenuItem, 1)
+    return `No one is eating our ${menuItemName} - it has been removed from the ${menuItemType} menu!`;
+  } else {
+    return `Sorry, we don't sell ${menuItemName}, try adding a new recipe!`
+  }
+};
 
 module.exports = {
   createRestaurant,
